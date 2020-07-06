56a91a678b1dddba0e5ddb397e909700818b30b3
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-06 15:23:16 -0400

    All routes and MVP

--- index.js
+++ index.js
@@ -1,8 +1,78 @@
 const express = require("express");
 const server = express();
+const shortid = require("shortid");
+server.use(express.json());
 
 server.get("/", (req, res) => {
   res.send("<h1>NodeProject1</h1>");
+});
+
+let users = [];
+server.post("/api/users", (req, res) => {
+  if (req.body.name === undefined || req.body.bio === undefined) {
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide a name and bio for the user." });
+  }
+
+  const newUser = req.body;
+  newUser.id = shortid.generate();
+  users.push(newUser);
+
+  res.status(201).json(newUser);
+});
+
+server.get("/api/users", (req, res) => {
+  res.json(users);
+  res
+    .status(500)
+    .json({ errorMessage: "The users information could not be retrieved." });
+});
+
+server.get("/api/users/:id", (req, res) => {
+  const user = users.find((user) => user.id === req.params.id);
+  if (!user) {
+    return res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  }
+  res.json(user);
+});
+
+server.delete("/api/users/:id", (req, res) => {
+  const user = users.find((user) => user.id === req.params.id);
+  if (!user) {
+    return res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  }
+  const newUsers = users.filter((user_ref) => {
+    return user_ref.id !== user.id;
+  });
+  users = newUsers;
+  res.json(user);
+});
+
+server.put("/api/users/:id", (req, res) => {
+  if (req.body.name === undefined || req.body.bio === undefined) {
+    return res
+      .status(400)
+      .json({ errorMessage: "Please provide a name and bio for the user." });
+  }
+  const user = users.find((user) => user.id === req.params.id);
+  if (!user) {
+    return res
+      .status(404)
+      .json({ message: "The user with the specified ID does not exist." });
+  }
+  const newUsers = users.filter((user_ref) => {
+    return user_ref.id !== user.id;
+  });
+  const newUser = req.body;
+  newUser.id = req.params.id;
+  newUsers.push(newUser);
+  users = newUsers;
+  res.status(200).json(newUser);
 });
 
 const PORT = 8000;
